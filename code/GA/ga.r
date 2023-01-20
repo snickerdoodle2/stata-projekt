7b71b70e21d73e163c724792b3e38ac590ff4a27
Dominik Pilipczuk <dominikpil123@gmail.com>
2023-01-20 21:02:49 +0100

    GA powinno byc poprawne 🥳🥳

--- code/GA/ga.r
+++ code/GA/ga.r
@@ -1,24 +1,23 @@
 library(GA)
+library(smoof)
 
 # TODO: correct GA parameters and/or set popSize & maxiter as arguments
 calculate_ga <- function(fitness, iters) {
-	lower <- getLowerBoxConstraints(fitness)
-	upper <- getUpperBoxConstraints(fitness)
+    lower <- getLowerBoxConstraints(fitness)
+    upper <- getUpperBoxConstraints(fitness)
 
-	out = c();
-
-	for (i in 1:iters){
-        single_GA <- ga(
+    single <- function() {
+        current_ga <- ga(
             type = "real-valued",
             fitness = function(x) (-1) * fitness(x),
             lower = lower,
             upper = upper,
             popSize = 50,
-            maxiter = 200
+            maxiter = 100
         )
 
-        out = c(out, fitness(single_GA@solution))
-	}
+        return(fitness(current_ga@solution))
+    }
 
-    return(mean(out))
+    return(mean(replicate(iters, single())))
 }
