7c7f42b096e1c8fc2a2d088ca67dcc933740cf01
Dominik Pilipczuk <dominikpil123@gmail.com>
2023-01-25 14:09:58 +0100

    Updated ga

--- code/GA/ga.r
+++ code/GA/ga.r
@@ -1,24 +1,26 @@
 library(smoof)
 library(ecr)
+library(mcreplicate)
 
 # TODO: correct GA parameters and/or set popSize & maxiter as arguments
 calculate_ga <- function(fitness, iters) {
-	lower = getLowerBoxConstraints(fitness)
-	upper = getUpperBoxConstraints(fitness)
+    lower <- getLowerBoxConstraints(fitness)
+    upper <- getUpperBoxConstraints(fitness)
+    dims <- getNumberOfParameters(fitness)
 
-	single_ga <- function() {
-		res <- ecr(
-			fitness.fun = fitness, representation = "float",
-			n.dim = getNumberOfParameters(fitness),
-			mu = 10L, lambda = 10L,
-			mutator = setup(mutGauss, lower = lower, upper = upper),
-			# terminators = list(stopOnIters(1000L))
-		)
+    single_ga <- function() {
+        res <- ecr(
+            fitness.fun = fitness, representation = "float",
+            n.dim = dims,
+            mu = 10L, lambda = 1L,
+            mutator = setup(mutGauss, sdev = 2, lower = lower, upper = upper),
+            terminators = list(stopOnIters(1000))
+        )
 
-		return(res$best.y)
-	}
+        return(res$best.y)
+    }
 
-	return(replicate(iters, single_ga()))
+    return(mc_replicate(iters, single_ga()))
 }
 
-print(calculate_ga(makeAckleyFunction(2), 50))
+print(calculate_ga(makeAckleyFunction(20), 50))
